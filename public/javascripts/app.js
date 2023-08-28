aaf6943b90c7430d20b8653241acf6e9353f2d04
Florian Nari <Florian.Nari@univ-paris1.fr>
2023-08-28 11:59:36 +0200

    allow_international_phone_number

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -137,7 +137,7 @@ const RandomCodeMethod = Vue.extend({
         saveTransport: function(transport) {
             var new_transport = document.getElementById(transport + '-input').value;
             var reg;
-            if (transport == 'sms') reg = new RegExp("^0[6-7]([-. ]?[0-9]{2}){4}$");
+            if (transport == 'sms') reg = new RegExp("^((0[67](([.]|[-]|[ ])?[0-9]){8})|((00|[+])(([.]|[-]|[ ])?[0-9]){7,15}))$");
             else reg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
             if (reg.test(new_transport)) {
                 $.ajax({
