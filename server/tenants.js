d06a4fb4ae634fe2af0f27e30c0612b971f6b094
Guillaume Rousse <guillaume.rousse@renater.fr>
2025-06-03 14:15:58 +0200

    there is no specific admin password anymore

--- server/tenants.js
+++ server/tenants.js
@@ -5,7 +5,7 @@ const passwords = new Map();
 async function getTenantByName(name) {
     const response = await fetch(properties.esup.api_url + '/admin/tenants', {headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + properties.esup.admin_password
+        'Authorization': 'Bearer ' + properties.esup.api_password
     }});
     const data = await response.json();
     return data.find(item => item.name === name);
@@ -14,7 +14,7 @@ async function getTenantByName(name) {
 async function getTenantById(id) {
     const response = await fetch(properties.esup.api_url + '/admin/tenant/' + id, {headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + properties.esup.admin_password
+        'Authorization': 'Bearer ' + properties.esup.api_password
     }});
     return await response.json();
 }
