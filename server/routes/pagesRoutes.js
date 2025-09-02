Renater/esup-otp-manager

commit 1b1afeb604597bdfaa45591eea2481b1e2c297d2

improve user search (breaking change, require esup-otp-api >= c369a76)
https://github.com/EsupPortail/esup-otp-api/commit/c369a7633f8b9b9f6782062770fbbf11e8e5b798

server/routes/pagesRoutes.js
@@ -2,6 +2,8 @@ import properties from '../../properties/properties.js';
 import * as utils from '../../services/utils.js';
 import logger from '../../services/logger.js';
 
+import { fetch_otp_api } from './apiRoutes.js';
+
 function isUser(req, res, next) {
     if (utils.isAuthenticated(req)) return next();
     res.redirect('/login'); // can't use 401 because of https://www.rfc-editor.org/rfc/rfc7235#section-3.1 (302 is used by default)
@@ -75,15 +77,13 @@ export function routing(router, passport) {
     } else if (properties.authentication.name == 'saml') {
 
         async function getUserActiveMethods(user) {
-            // TODO: do it on API-side
-            const response = await fetch(properties.esup.api_url + '/protected/users/' + user.uid, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + properties.esup.api_password
-                }
+            const response = await fetch_otp_api({
+                relUrl: '/protected/users/' + user.uid,
+                bearerAuth: true,
             });
             const data = await response.json();
 
+            // TODO: do it on API-side
             return Object.entries(data.user.methods)
                 .filter(([key, value]) => value.active)
                 .map(([key, value]) => key);

public/javascripts/app.js
@@ -1018,31 +1018,19 @@ var ManagerDashboard = Vue.extend({
                 methods: Object,
                 transports: Object
             },
-            uids: [],
+            users: [],
             requestedUid: '',
+            debounceTimeout: null,
         }
     },
-    computed: {
-        suggestions() {
-            return this.uids.filter(uid => uid.toLowerCase().includes(this.requestedUid.toLowerCase()));
-        },
-        requestedUidExists() {
-          return this.suggestions.includes(this.requestedUid);
-        },
-    },
-    created: function () {
-        if (this.uids?.length) {
-            return;
+    watch: {
+        requestedUid: function(newVal) {
+            clearTimeout(this.debounceTimeout);
+
+            this.debounceTimeout = setTimeout(() => {
+                this.fetchUsersByToken(newVal);
+            }, 300);
         }
-        return fetchApi({
-            method: "GET",
-            uri: "/api/admin/users",
-            onSuccess: res => {
-                this.uids = res.data.uids;
-            },
-        }).catch(err => {
-            toast(err, 3000, 'red darken-1');
-        });
     },
     mounted: async function() {
         const url = new URL(location);
@@ -1056,13 +1044,25 @@ var ManagerDashboard = Vue.extend({
         }
     },
     methods: {
+        fetchUsersByToken: function(token) {
+            if (token?.length < 3) {
+                this.users = [];
+                return;
+            }
+
+            fetchApi({
+                method: "GET",
+                uri: `/api/admin/users?token=${encodeURIComponent(token)}`,
+                onSuccess: res => {
+                    this.users = res.data.users;
+                }
+            }).catch(err => {
+                toast(err, 3000, 'red darken-1');
+            });
+        },
         search: function () {
             if (this.requestedUid) {
                 this.getUser(this.requestedUid);
-                if(!this.requestedUidExists) {
-                    this.uids.push.push(this.requestedUid);
-                    toast({ message: 'utilisateur ' + this.requestedUid + ' ajouté avec succès', className: 'green contrasted' });
-                }
                 this.requestedUid = '';
             }
         },
@@ -1212,7 +1212,6 @@ var app = new Vue({
             transports: {}
         },
         users_methods:{},
-        uids: [],
         messages: {},
         infos: {},
     },

server/routes.js
@@ -39,11 +39,37 @@ function routing() {
     apiRoutes.routing(router);
 }
 
+const attributesToSave = ["displayName"];
+
+function updateApiUser(user) {
+    if (!user.attributes) {
+        return;
+    }
+
+    const values = attributesToSave
+        .map(attr => [attr, user.attributes[attr.toLowerCase()]])
+        .filter(([attr, value]) => value);
+
+    if (!values) {
+        return;
+    }
+
+    return apiRoutes.fetch_otp_api({
+        method: 'PUT',
+        relUrl: '/protected/users/' + user.uid,
+        bearerAuth: true,
+        body: Object.fromEntries(values),
+    });
+}
+
 export default async function(_passport) {
     passport = _passport;
 
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
+        // async
+        updateApiUser(user);
+
         const _user = {
             uid:          user.uid,
             name:         user.name,
@@ -79,3 +105,5 @@ export default async function(_passport) {
 
     return router
 }
+
+

server/routes/apiRoutes.js
@@ -56,44 +56,68 @@ function canAccessUserMethod(req, res, next) {
     });
 }
 
-/** @param {{ relUrl: string, queryParams?: Object; bearerAuth?: true, method?: 'GET'|'POST'|'PUT'|'DELETE' }} opts_ */
-async function request_otp_api(req, res, opts_) {
+/** 
+ *  @typedef {{ 
+ *      relUrl: string,
+ *      queryParams?: Object,
+ *      body?: Object,
+ *      bearerAuth?: boolean,
+ *      headers?: Object,
+ *      method?: 'GET' | 'POST' | 'PUT' | 'DELETE',
+ *  }} opts_
+ */
+
+/**
+ * @param { opts_ } opts_
+ */
+export function fetch_otp_api(opts_) {
     logger.info("requesting api");
-    const clientIP = req.ip;
-    const userAgent = req.headers['user-agent'];
     /**
      * @typedef {import('undici').Dispatcher.RequestOptions} RequestOptions
      * @type {Omit<RequestOptions, 'origin' | 'path'>}
      */
-    let opts = {
+    const opts = {
         method: opts_.method || 'GET',
     }
 
-    if (req.body && Object.keys(req.body).length) {
-        opts.body = JSON.stringify(req.body);
+    if (opts_.body && Object.keys(opts_.body).length) {
+        opts.body = JSON.stringify(opts_.body);
     }
-    opts_.queryParams ||= {};
-    opts_.queryParams.managerUser = req.session.passport.user.uid;
 
     const url = properties.esup.api_url + opts_.relUrl + "?" + new URLSearchParams(opts_.queryParams);
 
-    opts.headers = {
-        'X-Client-IP': clientIP,
-        'Client-User-Agent': userAgent,
-        'Content-Type': 'application/json'
-    };
+    opts.headers = opts_.headers || {};
+    opts.headers['Content-Type'] ||= 'application/json';
 
     if (opts_.bearerAuth) {
         opts.headers.Authorization = 'Bearer ' + properties.esup.api_password;
     }
 
-    logger.debug(opts.method + ':' + opts.url);
-    logger.debug(req.session.passport)
+    logger.debug(opts.method + ':' + url);
     logger.debug(JSON.stringify(opts.headers, null, 2));
+
+    return request(url, opts);
+}
+
+/** @param {Omit<opts_, 'body' | 'headers'>} opts_ */
+async function request_otp_api(req, res, opts_) {
+    const clientIP = req.ip;
+    const userAgent = req.headers['user-agent'];
+
+    opts_.body = req.body;
+    opts_.queryParams ||= {};
+    opts_.queryParams.managerUser = req.session.passport.user.uid;
+
+    opts_.headers = {
+        'X-Client-IP': clientIP,
+        'Client-User-Agent': userAgent,
+    };
+
+    logger.debug(req.session.passport)
 
     let response;
     try {
-        response = await request(url, opts);
+        response = await fetch_otp_api(opts_);
     } catch (error) {
         res.status(503);
         return res.send({
@@ -101,8 +125,8 @@ async function request_otp_api(req, res, opts_) {
             "message": error.message || "Api did not give a response"
         });
     }
-    
-    
+
+
     // forward the status code, because if the request failed
     // it should not be responding with 200 ("everything is fine !")
     //
@@ -275,8 +299,9 @@ export function routing(router) {
 
     router.get('/api/admin/users', isManager, function(req, res) {
         request_otp_api(req, res, {
-            relUrl: '/admin/users/',
+            relUrl: '/protected/users/',
             bearerAuth: true,
+            queryParams: { token: req.query.token },
         });
     });
 

views/templates/manager-dashboard.pug
@@ -8,11 +8,11 @@ script#manager-dashboard(type='text/x-template').
     </div>
     
     <a v-if="requestedUid" class="waves-effect waves-light btn col s4" @click="search">
-        {{requestedUidExists ? messages.api.action.select : messages.api.action.add}}
-        <i class="material-icons right" aria-hidden="true">{{requestedUidExists ? 'search' : 'add'}}</i>
+        {{messages.api.action.select}}
+        <i class="material-icons right" aria-hidden="true">search</i>
     </a>
     <datalist id="usersId">
-    <option v-for="uid in suggestions" :value="uid"/>
+    <option v-for="user in users" :value="user.uid" :label="user.displayName ? `${user.displayName} (${user.uid})` : user.uid"/>
     </datalist>
     </div>
     <div v-show="user.uid">

commit c54d5f6f3a45f9e0b36186c3072cc88732b01c0b

fix SAML authentication
broken by https://github.com/EsupPortail/esup-otp-manager/commit/1b1afeb604597bdfaa45591eea2481b1e2c297d2#diff-bcbd702d444919d3df2c19657e30b5ef0ec224689e1a051920d10a97ba56583eR84

server/routes/pagesRoutes.js
@@ -81,7 +81,7 @@ export function routing(router, passport) {
                 relUrl: '/protected/users/' + user.uid,
                 bearerAuth: true,
             });
-            const data = await response.json();
+            const data = await response.body.json();
 
             // TODO: do it on API-side
             return Object.entries(data.user.methods)